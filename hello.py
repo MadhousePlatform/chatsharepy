fcb08744bdeb4c5e2f88412d3da3bbf01cf5a8c4
Steve <steve@vtc-drivershub.com>
2025-07-06 13:49:06 +0100

    Fixed linting

--- hello.py
+++ hello.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
+"""
+This is a simple hello world program.
+"""
+
 def main():
+    """
+    This is the main function.
+    """
     print("Hello, World!")
 
 if __name__ == "__main__":
-    main() 
+    main()
