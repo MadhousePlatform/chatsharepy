be7306803515cdf6116ad438b3fb4834d3fb0aa0
Sketch <sketch@sketchni.uk>
2026-09-10 22:21:03 +0100

    Make a debug message give only relevant information Parse arguments so that debug can be read. Add more required env vars and use the correct method to read envvars from dotenv

--- src/chatshare.py
+++ src/chatshare.py
@@ -5,15 +5,17 @@ Chatshare - A chat sharing application.
 """
 import os
 
-from src.debug import is_debug
+from src.debug import parse_args
 from src.pelican_manager import Pelican
 from src.websockets import Websockets
 from src.discord_client import DiscordClient
 from src.events import EventEmitter
 
 REQUIRED_ENV_VARS = [
+    'PANEL_ORIGIN_URL',
     'PANEL_API_URL',
     'PANEL_WSS_URL',
+    'WINGS_TOKEN',
     'PANEL_APPLICATION_KEY',
     'PANEL_CLIENT_KEY',
     'DISCORD_TOKEN',
@@ -21,7 +23,7 @@ REQUIRED_ENV_VARS = [
 ]
 
 for var in REQUIRED_ENV_VARS:
-    value = os.environ.get(var)
+    value = os.getenv(var)
     if not value:  # catches None and empty string
         raise ValueError(f"Please set the {var} environment variable.")
 
@@ -30,13 +32,13 @@ def main():
     """
     Main entry point for the Chatshare application.
     """
+    parse_args()
     print("Welcome to Chatshare!")
 
     # Get all servers
     pelican = Pelican()
     for server in pelican.get_servers():
-        if is_debug():
-            print(server)
+        print(f"server: {server.get('external_id')} - {server.get('name')} - {server.get('description')}")
         Websockets(server).connect_to_server(server)
 
     # Initialise the event emitter
